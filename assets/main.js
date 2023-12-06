46dc77aab3e17b3d17620d468eadbfa5d116d925
Andrea <123306195+bullbatti@users.noreply.github.com>
2023-12-06 06:55:18 +0100

    Transitions added

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,6 @@
+/**********************************************
+* Hamburger Menu
+**********************************************/
 const header = document.getElementById('header');
 const toggle = document.getElementById('toggle');
 const navbar = document.getElementById('navbar');
@@ -12,3 +15,22 @@ toggle.onclick = function(){
     toggle.classList.toggle('active');
     navbar.classList.toggle('active');
 }
+
+/**********************************************
+* Animation on scroll
+***********************************************/
+const observer = new IntersectionObserver((enteries) => {
+    enteries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
